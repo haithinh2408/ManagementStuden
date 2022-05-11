78ad007fc42e4800f00f3734f538003fb5bf64ca
Thinh Hai <haithinh23082000@gmail.com>
2022-05-11 11:50:46 +0700

    first commit

--- Configs/ConnectDb.php
+++ Configs/ConnectDb.php
@@ -2,9 +2,9 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpassword = "";
-$dbmanagementstudent = "managementstudent";
+$dbmanagementstudent = "MANAGEMENTSTUDENT";
 // connectObject quản lý chuỗi kết nối my sql
-$connect = new mysqli($dbhost, $dbuser, $dbpassword, $dbmanagementstudent);
+$connect = new mysqli($dbhost, $dbuser, $dbpassword, $dbMANAGEMENTSTUDENT);
 if($connect->connect_error){
     die("connection die".$connect->connect_error);
 }
